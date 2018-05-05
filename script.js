92fa91b4a7a1bd16904e02677dba6e0378511739
Zane Suhadolnik <zanesuhi@gmail.com>
2018-05-05 08:03:34 +0200

    Initiating a new Vue instance and reactivity system

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
+// Initiating a new Vue instance:
+var vm = new Vue ({
+  //options object
+  //vm -> short for ViewModel
+})
+
+// Reactivity system
+// Our data object
+var data = { a: 1 }
+
+// The object is added to a Vue instance
+var vm = new Vue({
+  data: data
+})
+
+// Getting the property on the instance
+// returns the one from the original data
+vm.a == data.a // => true
+
+// Setting the property on the instance
+// also affects the original data
+vm.a = 2
+data.a // => 2
+
+// ... and vice-versa
+data.a = 3
+vm.a // => 3
